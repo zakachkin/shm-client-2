124a276a94ce2258ee83566dfb95b842bb15940e
zakachkin <67714481+zakachkin@users.noreply.github.com>
2026-05-15 19:24:38 +0300

    Разместить кнопку перехода в бот под входом
    Добавлены стили для отображения кнопки перехода в бот под кнопкой входа на экране авторизации.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './i18n'
+import './login-support-button.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
